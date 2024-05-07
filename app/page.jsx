039d6fcbe51c0791fc94a7ab0c89e9ceb9cf37fa
jseam2 <hello.jseam@gmail.com>
2024-05-08 01:59:15 +0800

    fix: fix start state

--- app/page.jsx
+++ app/page.jsx
@@ -49,7 +49,7 @@ export default function Page() {
   // |___________________if recipe_id in localstorage__________________|
   //
   //
-  const [state, setState] = useState("processing")
+  const [state, setState] = useState("start")
   const [recording, setRecording] = useState(false)
   const [stream, setStream] = useState(null)
   const [audioChunks, setAudioChunks] = useState([])
